6cdb4bcdcbb2f500450225bf17cb488a6d4dfe90
vadimistar <vadimstar@gmail.com>
2023-11-08 20:27:47 +0300

    Init repository and create properties for db connection

---
+++ src/main/java/com/vadimistar/weatherviewer/WeatherviewerApplication.java
@@ -0,0 +1,13 @@
+package com.vadimistar.weatherviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherviewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vadimistar/weatherviewer/WeatherviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadimistar.weatherviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
