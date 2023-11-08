a773f284b12cb12cf9e4c6f1b0a465bd94d89b34
vadimistar <vadimstar@gmail.com>
2023-11-08 22:40:05 +0300

    Implement error exception handling

---
+++ src/main/java/com/vadimistar/weatherviewer/exceptions/ErrorDto.java
@@ -0,0 +1,13 @@
+package com.vadimistar.weatherviewer.exceptions;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ErrorDto {
+    String error;
+}
