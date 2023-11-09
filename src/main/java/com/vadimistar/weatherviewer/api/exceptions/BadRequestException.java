9a413488a4fab1b802dbc281812dcec64ff08d44
vadimistar <vadimstar@gmail.com>
2023-11-09 23:05:36 +0300

    remove unnecessary field description

--- src/main/java/com/vadimistar/weatherviewer/api/exceptions/BadRequestException.java
+++ src/main/java/com/vadimistar/weatherviewer/api/exceptions/BadRequestException.java
@@ -3,7 +3,7 @@ package com.vadimistar.weatherviewer.api.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
